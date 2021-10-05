abe7e94694d5074cffb030e60d9a991289d95ca9
sboamah <sboamah@stevens.edu>
2021-10-05 18:40:30 -0400

    started tabs

--- data-do-up/src/app/app.module.ts
+++ data-do-up/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataTabsComponent } from './data-tabs/data-tabs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTabsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ data-do-up/src/app/data-tabs/data-tabs.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-data-tabs',
+  templateUrl: './data-tabs.component.html',
+  styleUrls: ['./data-tabs.component.css']
+})
+export class DataTabsComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
