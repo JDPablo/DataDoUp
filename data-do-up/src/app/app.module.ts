0929583aed2d56ab8788fa0c28a876485fb47414
Kushal Patel <kushal@kushals-mbp.myfiosgateway.com>
2021-10-10 22:24:03 -0400

    Fixed login page, fixed some routing issues, made tabs only visable on second page

--- data-do-up/src/app/app.module.ts
+++ data-do-up/src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
