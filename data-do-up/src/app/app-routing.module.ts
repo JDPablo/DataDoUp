cb7fb03e5d287df7013e4f3bbef39b4a56d7b9c9
Sdrucker6637 <Spencerd6637@gmail.com>
2021-10-20 13:34:19 -0400

    Added four tabs pages and restyled buttons

--- data-do-up/src/app/app-routing.module.ts
+++ data-do-up/src/app/app-routing.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DataComponent } from './data/data.component';
+import { ReportsComponent } from './reports/reports.component';
+import { RawDataComponent } from './raw-data/raw-data.component';
+import { InsightsComponent } from './insights/insights.component';
+import { ReccomendationsComponent } from './reccomendations/reccomendations.component';
 
-const routes: Routes =[{path: '', component: LoginComponent}, {path: 'data', component: DataComponent}];
+const routes: Routes =[{path: '', component: LoginComponent}, {path: 'data', component: DataComponent}, 
+  {path: 'data/reports', component: ReportsComponent}, {path: 'data/raw-data', component: RawDataComponent},
+  {path: 'data/insights', component: InsightsComponent}, 
+  {path: 'data/reccomendations', component: ReccomendationsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- data-do-up/src/app/app.module.ts
+++ data-do-up/src/app/app.module.ts
@@ -6,18 +6,29 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DataComponent } from './data/data.component';
 import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { ReportsComponent } from './reports/reports.component';
+import { ReccomendationsComponent } from './reccomendations/reccomendations.component';
+import { InsightsComponent } from './insights/insights.component';
+import { RawDataComponent } from './raw-data/raw-data.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DataComponent
+    DataComponent,
+    ReportsComponent,
+    ReccomendationsComponent,
+    InsightsComponent,
+    RawDataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
