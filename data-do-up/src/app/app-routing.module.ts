f9b74b7b11b95980a365889318b1a59bd1cc5dce
Sdrucker6637 <Spencerd6637@gmail.com>
2021-10-25 21:36:01 -0400

    Fixed logout button and removed other buttons

--- data-do-up/src/app/app-routing.module.ts
+++ data-do-up/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DataComponent } from './data/data.component';
 import { DataTabsComponent } from './data-tabs/data-tabs.component';
+import { ReportsComponent } from './reports/reports.component';
 
 const routes: Routes =[{path: '', component: LoginComponent}, 
 {path: 'data', component: DataComponent},
-{path: 'datatabs', component: DataTabsComponent}
+{path: 'datatabs', component: DataTabsComponent},
+{path: 'data/reports', component: ReportsComponent},
+{path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
